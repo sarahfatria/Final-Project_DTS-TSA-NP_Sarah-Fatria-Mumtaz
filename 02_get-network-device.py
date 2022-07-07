16953db385fe33f4e90d87d4d2264ef241a35d25
sarahfatria <sarahfatria01@mail.ugm.ac.id>
2022-07-07 15:24:40 +0700

    Telah menambahkan file text kkonfigurasi network device, pkt, dan script python

---
+++ 02_get-network-device.py
@@ -0,0 +1,15 @@
+import json
+import requests
+api_url = "http://localhost:58000/api/v1/network-device"
+
+headers={"X-Auth-Token": "NC-16-2b4fd20d016248fcaafe-nbi"}
+
+resp = requests.get(api_url, headers=headers, verify=False)
+
+print("Request status: ", resp.status_code)
+
+response_json = resp.json()
+networkDevices = response_json["response"]
+
+for networkDevice in networkDevices:
+    print(networkDevice["hostname"], "\t", networkDevice["platformId"], "\t", networkDevice["managementIpAddress"])
